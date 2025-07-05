6042943b03724275b3ee323efbff26d0498a6d18
mjlavadia <mlavadia@byupathway.edu>
2025-07-05 22:42:43 +0800

    Exercise 1 to 5 done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int mystery_number = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != mystery_number)
+        {
+            Console.Write("Guess the mystery number: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (mystery_number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (mystery_number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+
+        }                    
     }
 }
