f3ae58138d5da5b54ce0de45c6e001115d9ae46a
mjlavadia <mlavadia@byupathway.edu>
2025-07-16 22:51:12 +0800

    Fractions

---
+++ week03/Fractions/Fractions.cs
@@ -0,0 +1,55 @@
+using System.Buffers.Binary;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int value)
+    {
+        _top = value;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int value)
+    {
+        _bottom = value;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}
